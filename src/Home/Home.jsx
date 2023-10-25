c8e4991cf91c485110b8428f154306dd8108c917
505 <505@academicos.cesde.edu.co>
2023-10-25 12:23:12 -0500

    primer compromiso

---
+++ src/Home/Home.jsx
@@ -0,0 +1,15 @@
+import'./Home.css'
+import{Menu} from'../Menu/Menu'
+export function Home(){
+    return(
+        <>
+        <Menu></Menu>
+        <section className='banner'>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius quidem laborum temporibus eos voluptatem, unde asperiores ex 
+                perspiciatis nobis ipsum iste animi, sapiente beatae at corrupti facere similique, expedita officiis.</p>
+
+        </section>
+
+    </>
+    )
+}
